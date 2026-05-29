d430c0a2ace22dbeacdf30d15fd3198ca5d3c014
SPRAGE <46289351+SPRAGE@users.noreply.github.com>
2026-05-29 07:16:56 +0000

    chore: update ruflo to v3.10.5

--- flake.nix
+++ flake.nix
@@ -16,16 +16,16 @@
         packages = {
           ruflo = pkgs.buildNpmPackage rec {
             pname = "ruflo";
-            version = "3.5.14";
+            version = "3.10.5";
 
             src = pkgs.fetchFromGitHub {
               owner = "ruvnet";
               repo = "ruflo";
               rev = "v${version}";
-              hash = "sha256-z4XpsMAopEdSuaM3f7iN3Xq2I30Izl6t8WPNPlHVXzY=";
+              hash = "sha256-j0Q2e7skENnMe6hyNiWzgo1FqhxyvIDb01/QPiiypxk=";
             };
 
-            npmDepsHash = "sha256-BnzVQ8XALE0I6Ra/YJG9JfEs+7QpHmJkgE1PyApLL4M=";
+            npmDepsHash = "sha256-XWAuyNxpr1OT5CG3FnExoko6yYdzALN3f3CEEigqBrk=";
 
             inherit nodejs;
 
