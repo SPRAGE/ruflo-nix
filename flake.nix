afa3cbc500603195fa174e9d613290ce520f95dd
SPRAGE <46289351+SPRAGE@users.noreply.github.com>
2026-07-14 06:43:42 +0000

    chore: update ruflo to v3.28.0

--- flake.nix
+++ flake.nix
@@ -16,16 +16,16 @@
         packages = {
           ruflo = pkgs.buildNpmPackage rec {
             pname = "ruflo";
-            version = "3.5.14";
+            version = "3.28.0";
 
             src = pkgs.fetchFromGitHub {
               owner = "ruvnet";
               repo = "ruflo";
               rev = "v${version}";
-              hash = "sha256-z4XpsMAopEdSuaM3f7iN3Xq2I30Izl6t8WPNPlHVXzY=";
+              hash = "sha256-ITMBSOXMY9f+h+k2ZyPwgckjvGsn9wfBpLgwXTTCpco=";
             };
 
-            npmDepsHash = "sha256-BnzVQ8XALE0I6Ra/YJG9JfEs+7QpHmJkgE1PyApLL4M=";
+            npmDepsHash = "sha256-OSQhswOoOUqKjiZbaagzPyeus0UJXcP6dFvS4GFjGck=";
 
             inherit nodejs;
 
